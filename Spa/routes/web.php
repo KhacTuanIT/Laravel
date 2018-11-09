fceda42c9beb2397e671b5e78fb4198d57aa8f56
HungThinh0710 <nhtnokia@gmail.com>
2018-11-09 19:44:11 +0700

    Add SpaManagementSys

--- Spa/routes/web.php
+++ Spa/routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login',function(){
-	return view('login');
+Route::group(['prefix' => 'admincp'],function(){
+	Route::get('/','BackEnd\AdminCPLogin@showLogin')->name('admincp_showLogin');
+	Route::get('login','AdminCP\AdminCPLogin@showLogin')->name('admincp_showLogin');
+
+	Route::group(['prefix' => 'spa'],function(){
+		Route::get('/','AdminCP\SpaManagementSystem\DashboardController@showDashBoard')->name('spa_showDashBoard');
+		Route::get('/dashboard','AdminCP\SpaManagementSystem\DashboardController@showDashBoard')->name('spa_showDashBoard');
+	});
 });
